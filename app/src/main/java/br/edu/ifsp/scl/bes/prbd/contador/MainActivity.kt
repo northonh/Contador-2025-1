24fc6c2e962e24217bd9c1a6052c45ab2873a8ff
northonh <nobile.pedro@gmail.com>
2025-02-21 08:47:09 -0300

    Aula 21/02/2025 - Adicionando EditText para valor inicial e usando TextChangedListener para setar o valor inicial.

--- app/src/main/java/br/edu/ifsp/scl/bes/prbd/contador/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prbd/contador/MainActivity.kt
@@ -2,12 +2,15 @@ package br.edu.ifsp.scl.bes.prbd.contador
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     private lateinit var cliqueBt: Button
     private lateinit var contadorTv: TextView
+    private lateinit var inicialEt: EditText
     private var contador = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,25 @@ class MainActivity : AppCompatActivity() {
 
         cliqueBt = findViewById(R.id.cliqueBt)
         contadorTv = findViewById(R.id.contadorTv)
+        // Perceba que usamos uma forma diferente para nomear o id do EditText.
+        inicialEt = findViewById(R.id.inicial_et)
+
+/*        inicialEt.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // NSA
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // NSA
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                contador = p0.toString().toInt()
+            }
+        })*/
+
+        // A linha abaixo é equivalente ao modo Java de setar um TextChangedListener acima.
+        inicialEt.addTextChangedListener { contador = it.toString().toInt() }
 
         // Usando um lambda para setar o OnClickListener do Button
         cliqueBt.setOnClickListener { contadorTv.text = (++contador).toString() }
