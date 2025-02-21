88594ebf2aeaba97b928637a9c9a4677839a253a
northonh <nobile.pedro@gmail.com>
2025-02-21 10:40:44 -0300

    Aula 21/02/2025 - Usando Spinner para setar valor inicial.

--- app/src/main/java/br/edu/ifsp/scl/bes/prbd/contador/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prbd/contador/MainActivity.kt
@@ -2,9 +2,9 @@ package br.edu.ifsp.scl.bes.prbd.contador
 
 import android.os.Bundle
 import android.view.View
-import android.widget.CheckBox
+import android.widget.AdapterView
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
 import br.edu.ifsp.scl.bes.prbd.contador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +19,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(activityMainBinding.root)
 
         with (activityMainBinding) {
-            inicialCb.setOnClickListener {
-                contador = (it as CheckBox).text.toString().toInt()
+            inicialSp.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+//                    contador = resources.getStringArray(R.array.valores_iniciais)[position].toInt()
+                    contador = when(position) {
+                        0 -> { 0 }
+                        1 -> { 5 }
+                        2 -> { 10 }
+                        else -> { -1 }
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // NSA
+                }
             }
 
             // Usando um lambda para setar o OnClickListener do Button
